fa8d32a38b3a0844aec9eee0b87cb24f18474e8c
Ross Basri <rossbasri@Rosss-MBP.fios-router.home>
2022-01-19 17:11:16 -0500

    initial commit

---
+++ node_modules/forta-agent/dist/starter-project/py/src/agent.py
@@ -0,0 +1,42 @@
+from forta_agent import Finding, FindingType, FindingSeverity
+
+MEDIUM_GAS_THRESHOLD = 1000000
+HIGH_GAS_THRESHOLD = 3000000
+CRITICAL_GAS_THRESHOLD = 7000000
+
+findings_count = 0
+
+
+def handle_transaction(transaction_event):
+    findings = []
+    gas_used = int(transaction_event.gas_used)
+
+    # limiting this agent to emit only 5 findings so that the alert feed is not spammed
+    global findings_count
+    if findings_count >= 5:
+        return findings
+
+    if gas_used < MEDIUM_GAS_THRESHOLD:
+        return findings
+
+    findings.append(Finding({
+        'name': 'High Gas Used',
+        'description': f'Gas Used: {gas_used}',
+        'alert_id': 'FORTA-1',
+        'type': FindingType.Suspicious,
+        'severity': get_severity(gas_used),
+        'metadata': {
+            'gas_used': gas_used
+        }
+    }))
+    findings_count += 1
+    return findings
+
+
+def get_severity(gas_used):
+    if gas_used > CRITICAL_GAS_THRESHOLD:
+        return FindingSeverity.Critical
+    elif gas_used > HIGH_GAS_THRESHOLD:
+        return FindingSeverity.High
+    else:
+        return FindingSeverity.Medium
